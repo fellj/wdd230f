6ae47e75aac6118c2ab5d5e3e0794b1e9f1e2dd0
ellefell <fel21001@byui.edu>
2023-01-21 09:55:11 -0600

    lesson 3 original commit

---
+++ lesson3/scripts/app.js
@@ -0,0 +1,9 @@
+
+
+//copyright year
+const year =  new Date().getFullYear();
+document.querySelector("#copyyear").textContent = year;
+
+//Last updated logic
+let lastupdate = new Date(document.lastModified);
+document.querySelector("#lastupdate").innerHTML = lastupdate;

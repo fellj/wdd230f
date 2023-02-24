d0753456cc66556d8eca9cd73c6e84d0ab1f7fdc
ellefell <fel21001@byui.edu>
2023-02-24 13:02:28 -0600

    lesson08 table build

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,3 +1,20 @@
 const wchill = document.querySelector("#windchill");
-const temp = document.querySelector("#temp");
-const windSpeed = doument.querySelector("#windSpeed");
+const temp = parseFloat(document.querySelector("#temp").textContent);
+const windspeed = parseFloat(document.querySelector("#windspeed").textContent);
+
+let windchill;
+
+if (temp <= 50.0 && windspeed > 3.0) {
+    
+    windchill = Math.round(
+        35.74 + 0.6215 * temp - 35.75 * Math.pow(windspeed, 0.16) + 0.4275 * temp * Math.pow(windspeed, .16)
+    );
+
+     wchill.textContent = windchill;
+}
+else {
+    windchill = "N/A";
+    wchill.textContent = windchill;
+    
+}
+

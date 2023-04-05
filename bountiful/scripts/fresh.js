a10300e57c28b94bf9bdcb625905632859b0ff48
ellefell <fel21001@byui.edu>
2023-04-05 18:07:04 -0500

    m-update

--- bountiful/scripts/fresh.js
+++ bountiful/scripts/fresh.js
@@ -22,7 +22,7 @@ dropdown2.add(defaultOption2);
 dropdown2.selectedIndex = 0;
 
 dropdown3.add(defaultOption3);
-dropdown3.selectedIndex = 0;
+dropdown3.selectedIndex = 0; 
 
 const urlFresh = "https://ellefell.github.io/wdd230/bountiful/data.json";
 
@@ -44,22 +44,25 @@ fetch(urlFresh)
     	for (let i = 0; i < data.length; i++) {
           option = document.createElement('option');
       	  option.text = data[i].name;
-      	  option.value = data[i].abbreviation;
-/* 
-          option2 = document.createElement('option2');
-      	  option2.text = data[i].name;
-      	  option2.value = data[i].abbreviation;
-
-          option3 = document.createElement('option3');
-      	  option3.text = data[i].name;
-      	  option3.value = data[i].abbreviation;
- */
-
-
+      	  //option.value = data[i].abbreviation; THERE IS NOTHING IN ABBREVIATION
       	  dropdown.add(option);
-         /*  dropdown2.add(option2);
-          dropdown3.add(option3); */
-    	}    
+    	}
+        
+        /* Additional For Loop Fruit Option # 2 */
+        for (let i = 0; i < data.length; i++) {
+            option = document.createElement('option');
+            option.text = data[i].name;
+            dropdown2.add(option);
+        }
+
+        /* Additional For Loop Fruit Option # 3 */
+        for (let i = 0; i < data.length; i++) {
+            option = document.createElement('option');
+            option.text = data[i].name;
+            dropdown3.add(option);
+        }
+
+
         
       }
       );  
@@ -69,6 +72,76 @@ fetch(urlFresh)
     console.error('Fetch Error -', err);  
   });
 
+// Fetches data from the json source url using await.
+async function getFruitData() {
+    const response = await fetch(urlFresh);
+    const data = await response.json();
+    displayOrder(data.fruits);
+  }  
+  
+  getFruitData();
+  
+
+// Display the fetched json business data  
+const displayOrder = (businesses) => {
+
+    // Create a placeholder for the output business cards container element
+    const cards = document.querySelector('div.cards'); // select the output container element
+
+    // For each loop that builds prophet cards
+    businesses.forEach((business) => {
+
+        // Create elements to add to the div.cards element
+        let card     = document.createElement('section');
+        let h3       = document.createElement('h3');
+        let logo     = document.createElement('img');
+        let para1    = document.createElement('p');
+        let url      = document.createElement('a');
+
+
+        // Build the h2 content out to show the company name
+        // Finish the template string
+        h3.textContent = `${business.companyname} `;
+
+        // Build the paragraph content with contact name and phone number
+        para1.style.textAlign = "center";
+        para1.innerHTML       = `Contact Name: ${business.contactname}`;
+        para1.innerHTML      += " <br> "
+        para1.innerHTML      += `Phone Number: ${business.phone}`;
+        para1.innerHTML      += " <br> "
+        para1.innerHTML      += `Membership Level: ${business.membershiplevel}`;
+        para1.innerHTML      += " <br> "
+        para1.innerHTML      += `<a href="${business.businessurl}" target="_blank">Company Website</a>`;
+        
+        
+
+        // Build the company url
+  /*       url.setAttribute('href', business.businessurl);
+        url.setAttribute('target', "_blank"); */
+
+        url.href = business.url;
+        url.title = business.url;
+        url.textContent = business.url;
+
+        // Build the image portrait by setting all the relevant attributes
+        logo.setAttribute('src', business.imageurl);
+        logo.setAttribute('alt', `Logo for ${business.companyname} `);
+        logo.setAttribute('loading', 'lazy');
+        logo.setAttribute('width', '250');
+        logo.setAttribute('height', '300');
+        
+        
+        // Append the section(card) with the created elements
+        card.appendChild(h3);
+        card.appendChild(para1);
+        card.appendChild(url);
+        card.appendChild(logo);
+        
+        // Append the card to the cards output element
+        cards.appendChild(card);
+
+        }) // end of forEach loop
+    }     // end of function expression
 
   // Build the paragraph content with carbohydrates, protein, fat, sugar, and calories totaled
 /*   para1.style.textAlign = "center";
